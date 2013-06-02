c74856a457dd19c2bdd33e17670670a90effd1b6
Lars Yencken <lars@yencken.org>
2013-06-02 10:35:05 +1000

    Add base structure for wordcloud data.

--- src/serve.py
+++ src/serve.py
@@ -9,13 +9,42 @@ Run a bubble chart visualisation.
 """
 
 import flask
+import pandas as pd
+from os import path
 
 app = flask.Flask(__name__)
 
+DATA_FILE = path.join(path.dirname(__file__), '..', 'output',
+                      'speeches-plus.csv')
+
+
+@app.route('/')
+def index():
+    return flask.render_template('index.html')
+
 
 @app.route('/bubble')
-def bubble():
+def bubble_speakers():
+    d = pd.read_csv(DATA_FILE)
+    speakers = sorted(map(int, d.session_talker_id.unique()))
+    return flask.render_template('bubble_index.html', speakers=speakers)
+
+
+@app.route('/bubble/<int:speakerid>')
+def bubble_speaker(speakerid):
     return flask.render_template('bubble.html')
+
+
+@app.route('/wordcloud')
+def wordcloud_speakers():
+    d = pd.read_csv(DATA_FILE)
+    speakers = sorted(map(int, d.session_talker_id.unique()))
+    return flask.render_template('wordcloud_index.html', speakers=speakers)
+
+
+@app.route('/wordcloud/<int:speakerid>')
+def wordcloud_speaker(speakerid):
+    return 'Word cloud for speaker %d' % speakerid
 
 
 if __name__ == '__main__':
